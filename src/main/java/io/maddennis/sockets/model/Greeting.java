1bafb58617820d9a7dcc49b31ec10dbe8438018a
dennismasinde <chengwadennismasinde@gmail.com>
2023-05-03 17:03:05 +0300

    websocket comm using stomp

---
+++ src/main/java/io/maddennis/sockets/model/Greeting.java
@@ -0,0 +1,17 @@
+package io.maddennis.sockets.model;
+
+public class Greeting {
+
+    private String content;
+
+    public Greeting() {
+    }
+
+    public Greeting(String content) {
+        this.content = content;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
---
+++ src/main/java/io/maddennis/sockets/model/HelloMessage.java
@@ -0,0 +1,21 @@
+package io.maddennis.sockets.model;
+
+public class HelloMessage {
+
+    private String name;
+
+    public HelloMessage() {
+    }
+
+    public HelloMessage(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
